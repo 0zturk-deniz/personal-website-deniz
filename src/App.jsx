d4f52b2b27b9a9be0f525df282df11a9bf773cef
0zturk-deniz <ddeniz.ozturkk@gmail.com>
2025-04-28 15:44:35 +0300

    tailwind kuruldu

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Landing from './components/Landing'
+import "./index.css"
+
+function App() {
+  
+
+  return (
+    <>
+    <Landing/>
+      <h1 className='text-xl text-blue-500 bg-red-800'>ALLAH AŞKINA TAILWIND ÇALIŞIYOR MU NOLUR😮‍💨</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Landing.jsx
@@ -0,0 +1,5 @@
+export default function Landing() {
+    return (
+        <div className="bg-green-500">deniz deniz deniz</div>
+    )
+}
