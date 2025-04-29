38068aedc3293782f1541990b9ffc49ed70fefd4
0zturk-deniz <ddeniz.ozturkk@gmail.com>
2025-04-29 19:48:21 +0300

    fixes header background

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,18 @@
+import { FaToggleOn } from "react-icons/fa";
+
 export default function Header () {
     return (
-        <header className="flex justify-between items-center px-8 py-4 bg-transparent">
-            <p className="font-inter font-bold text-lg text-[#CBF281]">deniz</p>
-           <div className="flex items-center gap-4"><button className="font-inter text-[#CBF281] bg-transparent">TÜRKÇE'YE GEÇ</button>
-           <button className="font-inter text-[#4731D3] bg-transparent">DARK MODE</button></div> 
+        <header className="flex flex-row justify-between">
+            <div className="w-2/3 bg-[#4731D3] flex flex-col justify-center items-start gap-15">
+            <p className="font-inter font-semibold text-3xl text-[#CBF281] ml-40 mt-12">deniz</p>
+            </div>
+            <div className="w-1/3 bg-[#CBF281]">
+            <div className="flex flew-row gap-4 ml-[-150px] p-6">
+            <button className="font-inter font-semibold text-[#CBF281]">TÜRKÇE'YE GEÇ</button>
+           <button className="font-inter font-semibold text-[#4731D3]">DARK MODE</button>
+           </div> 
+           </div>
+           
         </header>
     )
 }
--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -6,12 +6,12 @@ export default function Landing() {
         <>
         <section className="flex justify-center">
             
-            <div className="w-2/3 h-max bg-[#4731D3] flex flex-col justify-center items-start p-40 gap-8">
+            <div className="w-2/3 h-max bg-[#4731D3] flex flex-col justify-center items-start p-40 gap-6">
 
             <h1 className="font-inter text-6xl font-semibold text-[#CBF281]">I am a Frontend <br/>Developer...</h1>
-            <p className="font-inter font-xl text-white">...who likes to craft solid and scalable frontend <br/>products with great user experiences.</p>
+            <p className="font-inter text-2xl text-white">...who likes to craft solid and scalable frontend <br/>products with great user experiences.</p>
             <div className="flex flex-row justify-center gap-4">
-            <button className="bg-white px-6 py-3 text-[#4731D3] font-inter rounded flex flex-row"><FaGithub className="text-xl"/>GitHub</button>
+            <button className="bg-white px-6 py-3 text-[#4731D3] font-inter rounded flex flex-row"><FaGithub className="text-xl"/>Github</button>
             <button className="bg-white px-6 py-3 text-[#4731D3] font-inter rounded flex flex-row"><FaLinkedin className="text-xl"/>Linkedin</button>
             </div>
             </div>
